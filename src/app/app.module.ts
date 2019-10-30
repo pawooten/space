0c041c7003ed663473bbbf6678482a4157e8efd9
Paul Wooten <pwooten@OnBase.net>
2019-10-30 12:07:36 -0400

    angular update work-in-progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 // import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -20,7 +19,6 @@ import { DataLoaderService } from './services/data-loader.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule//,
     // HttpClientModule
   ],
   providers: [ AppService, GameService, ScoreService, ImageLoaderService, DataLoaderService ],
